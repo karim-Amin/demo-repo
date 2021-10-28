b5234702d57f40ad85c22d3578a7db0f8d784836
Kareeem <kareem.asu11@gmail.com>
2021-10-28 22:55:36 +0200

    here is updating

--- main.c
+++ main.c
@@ -2,7 +2,10 @@ int main(void){
     /*some initialization code*/
     /*super loop*/
     while(1){
-        
+        int x = 2;
+        if(x>2){
+            x = 0;
+        }
     }
     return 0;
 }
